bf8969142b3b785583097aae97627d7c5f4f2b5e
Evgeny Gavriluk <evgeny_gavriluk@mail.ru>
2023-10-16 19:06:10 +0300

    Iteration 2. testing HTTP saerver

---
+++ cmd/shortener/main_test.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+    "github.com/stretchr/testify/assert"
+    "net/http"
+    "net/http/httptest"
+    "testing"
+    "strings"
+    "fmt"
+)
+
+func TestHandlerPost(t *testing.T){
+    urls = make(map[string]string)
+    
+    type want struct {
+        code int
+      }
+    tests := []struct {
+        name string
+        body string
+        want want
+    }{
+        {
+            name: "post test 1. body doesn't consist of data",
+            body: "",
+            want: want{
+                code: 400,
+            },
+        },
+        {
+            name: "post test 2. body consist of data",
+            body: "http://ya.ru",
+            want: want{
+                code: 201,
+            },
+
+        },
+    }
+
+    for _, test := range tests {
+        t.Run(test.name, func(t *testing.T) {
+            fmt.Printf("\n\nTest %v Body %v\n", test.name, test.body)
+            body := strings.NewReader(test.body)
+            request := httptest.NewRequest(http.MethodPost, "/", body)
+            w := httptest.NewRecorder()
+            handlerPost(w, request)
+
+            res := w.Result()
+            fmt.Printf("want code = %d StatusCode %d\n", test.want.code, res.StatusCode)
+            assert.Equal(t, test.want.code, res.StatusCode)
+        })
+    }
+    
+}
+
+func TestHandlerGet(t *testing.T){
+    type want struct {
+        code int
+    }
+    tests := []struct {
+        name string
+        body string
+        want want
+    }{
+        {
+            name: "get test 1. body doesn't consist of data",
+            body: "",
+            want: want{
+                code: 400,
+            },
+        },
+        {
+            name: "get test 2. body consist of data",
+            body: "http://ya.ru",
+            want: want{
+                code: 307,
+            },
+
+        },
+    }
+
+    for _, test := range tests {
+        t.Run(test.name, func(t *testing.T) {
+            var addr string
+            if test.body == "/"{
+                addr =""
+            } else {
+                for k, v := range urls {
+                    if v == test.body {
+                        addr = k
+                    }
+                }
+            }
+            fmt.Printf("\n\nTest %v Body %v Addr %v\n", test.name, test.body, addr)
+            request := httptest.NewRequest(http.MethodGet, "/"+addr, nil)
+            w := httptest.NewRecorder()
+            handlerGet(w, request)
+
+            res := w.Result()
+            fmt.Printf("want code = %d StatusCode %d\n", test.want.code, res.StatusCode)
+            assert.Equal(t, test.want.code, res.StatusCode)
+        })
+    }
+
+}
