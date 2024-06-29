873ce697b7b5bd16c7b81fb5054d6899e1e461a3
IgorFed <greusunset@mail.ru>
2024-06-29 14:24:12 +0700

    Add server and shortURL, fullURL endpoints)

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,54 @@
 package main
 
-func main() {}
+import (
+	"io"
+	"net/http"
+	"net/url"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, shortURL)
+	mux.HandleFunc(`/{id}/`, fullURL)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil{
+		panic(err)
+	}
+}
+
+func shortURL(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		res.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	reqBody, err := io.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = url.ParseRequestURI(string(reqBody))
+	if err != nil{
+		res.WriteHeader(http.StatusBadRequest)
+		res.Write([]byte{})
+	}
+
+	res.Header().Set("Content-type", "text/plain")
+
+	res.WriteHeader(http.StatusCreated)
+	resBody := `http://localhost:8080/EwHXdJfB `
+	res.Write([]byte(resBody))
+}
+
+func fullURL(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		res.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	res.Header().Set("Location", "https://practicum.yandex.ru/")
+	res.WriteHeader(http.StatusTemporaryRedirect)
+	res.Write([]byte{})
+}
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module shortener
+
+go 1.22.2
