00ca891f3b22987c161e921dca9119831eb2f016
IgorFed <greusunset@mail.ru>
2024-06-29 15:52:45 +0700

    fix handlers

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,9 +2,29 @@ package main
 
 import (
 	"io"
+	"math/rand"
 	"net/http"
 	"net/url"
+	"strings"
+	"strconv"
 )
+
+//Переменные используем в качестве БД
+var index []string
+var db map[string]string
+
+//generateShort генерирует строку, которая будет использоваться длясокращения URL
+func generateShort() string{
+	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+    "abcdefghijklmnopqrstuvwxyz" +
+    "0123456789~=+%^*/()[]{}/!@#$?|")
+	length := 8
+	var b strings.Builder
+	for i := 0; i < length; i++ {
+    	b.WriteRune(chars[rand.Intn(len(chars))])
+	}
+	return b.String()
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -17,6 +37,7 @@ func main() {
 	}
 }
 
+//shortURl обрабатывает запрос для генерации короткого URL
 func shortURL(res http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodPost {
 		res.WriteHeader(http.StatusMethodNotAllowed)
@@ -37,7 +58,14 @@ func shortURL(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-type", "text/plain")
 
 	res.WriteHeader(http.StatusCreated)
-	resBody := `http://localhost:8080/EwHXdJfB `
+
+	//генерируем строку для URL
+	short := generateShort()
+
+	//записываем в "БД"
+	index = append(index, short)
+	db[short] = string(reqBody)
+	resBody := `http://localhost:8080/` + short
 	res.Write([]byte(resBody))
 }
 
@@ -47,7 +75,30 @@ func fullURL(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	res.Header().Set("Location", "https://practicum.yandex.ru/")
+	//Получаем путь из запроса, достаем id и переводим в int
+	path := req.URL.Path
+	
+	num := strings.Split(path, "/")
+	id, err := strconv.Atoi(num[1])
+	if err != nil{
+		res.WriteHeader(http.StatusBadRequest)
+	}
+
+	//проверка, что есть короткая ссылка с таким id
+	if id < 0 || id >= len(index) {
+		res.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	//получаем из БД сначала короткую ссылку по индексу, а затем полную
+	short := index[id]
+	full, ok := db[short]
+	if !ok {
+		res.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	res.Header().Set("Location", full)
 	res.WriteHeader(http.StatusTemporaryRedirect)
 	res.Write([]byte{})
 }
