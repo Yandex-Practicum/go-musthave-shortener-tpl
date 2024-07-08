74d25d9ff7f88f5ec76278c99ea5faeb5acfd159
IgorFed <greusunset@mail.ru>
2024-07-08 15:29:17 +0700

    add work with env

--- cmd/config/flags.go
+++ cmd/config/flags.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 var (
@@ -13,6 +14,14 @@ func ParseFlag() {
 	flag.StringVar(&Serv, "a", "localhost:8080", "address  to run server")
 	flag.StringVar(&Base, "b", "http://localhost:8080", "base address for short URL")
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("SERVER_ADDRESS"); envRunAddr != "" {
+		Serv = envRunAddr
+	}
+
+	if envBaseAddr := os.Getenv("BASE_URL"); envBaseAddr != "" {
+		Base = envBaseAddr
+	}
 
 	if Serv == "" {
 		Serv = "localhost:8080"
