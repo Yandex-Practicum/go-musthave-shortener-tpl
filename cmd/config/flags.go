6a4d37f02745aba7abc88f6c48da02d66faebd76
IgorFed <greusunset@mail.ru>
2024-07-13 13:13:43 +0700

    fix flags

--- cmd/config/flags.go
+++ cmd/config/flags.go
@@ -5,30 +5,33 @@ import (
 	"os"
 )
 
+const (
+	defaultServ = "localhost:8080"
+	defaultBase =  "http://localhost:8080"
+)
+
 var (
 	Serv string
 	Base string
 )
 
 func ParseFlag() {
-	flag.StringVar(&Serv, "a", "localhost:8080", "address  to run server")
-	flag.StringVar(&Base, "b", "http://localhost:8080", "base address for short URL")
+
+	Serv = os.Getenv("SERVER_ADDRESS")
+	Base = os.Getenv("BASE_URL")
+	
+
+	servFlag := flag.String("a", defaultServ, "address  to run server")
+	baseFlag := flag.String("b", defaultBase, "base address for short URL")
 	flag.Parse()
 
-	if envRunAddr := os.Getenv("SERVER_ADDRESS"); envRunAddr != "" {
-		Serv = envRunAddr
-	}
-
-	if envBaseAddr := os.Getenv("BASE_URL"); envBaseAddr != "" {
-		Base = envBaseAddr
-	}
 
 	if Serv == "" {
-		Serv = "localhost:8080"
+		Serv = *servFlag
 	}
 
 	if Base == "" {
-		Base = "http://localhost:8080"
+		Base = *baseFlag
 	}
 
 }
