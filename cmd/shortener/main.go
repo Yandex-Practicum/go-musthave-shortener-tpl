6bb2c12c92c1a48fdc9a91215ea8d41bf7060b15
IgorFed <greusunset@mail.ru>
2024-07-18 22:10:18 +0700

    add logger

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,18 +3,30 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/IgorGreusunset/shortener/cmd/config"
 	model "github.com/IgorGreusunset/shortener/internal/app"
 	"github.com/IgorGreusunset/shortener/internal/handlers"
 	"github.com/IgorGreusunset/shortener/internal/storage"
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 )
 
 
-
+var sugar zap.SugaredLogger
 
 func main() {
+
+	logger, err := zap.NewDevelopment()
+
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+
+	sugar = *logger.Sugar()
+
 	router := chi.NewRouter()
 
 	db := storage.NewStorage(map[string]model.URL{})
@@ -28,6 +40,8 @@ func main() {
 		handlers.GetByIDHandler(db, res, req)
 	}
 
+	router.Use(WithLogging)
+
 	router.Post(`/`, PostHandlerWrapper)
 	router.Get(`/{id}`, GetHandlerWrapper)
 
@@ -38,3 +52,59 @@ func main() {
 	log.Fatal(http.ListenAndServe(serverAdd, router))
 }
 
+func WithLogging(h http.Handler) http.Handler {
+	logFn := func (res http.ResponseWriter, req *http.Request)  {
+		start := time.Now()
+
+		responseData := &responseData{
+			status: 0,
+			size: 0,
+		}
+
+		lw := loggingResponseWriter{
+			ResponseWriter: res,
+			responseData: responseData,
+		}
+
+		uri := req.RequestURI
+		method := req.Method
+
+		h.ServeHTTP(&lw, req)
+
+		duration := time.Since(start)
+
+		sugar.Infoln(
+			"uri", uri,
+			"method", method,
+			"status", responseData.status,
+			"duration", duration,
+			"size", responseData.size,
+		)
+		
+	}
+	return http.HandlerFunc(logFn)
+} 
+
+
+type (
+	responseData struct {
+		status int
+		size int
+	}
+
+	loggingResponseWriter struct {
+		http.ResponseWriter
+		responseData *responseData
+	}
+)
+
+func (r *loggingResponseWriter) Write (b []byte) (int, error) {
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(starusCode int) {
+	r.ResponseWriter.WriteHeader(starusCode)
+	r.responseData.status = starusCode
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,10 @@ go 1.22.2
 require (
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/go-resty/resty/v2 v2.13.1
+	go.uber.org/zap v1.27.0
 )
 
-require golang.org/x/net v0.25.0 // indirect
+require (
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+)
