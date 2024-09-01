a0f5321932e75653173c0a4d73542a4104c7d338
Pavel <kamencov@gmail.com>
2024-09-01 12:25:59 +0300

    iter15 finish

--- cmd/shortener/initDB.go
+++ cmd/shortener/initDB.go
@@ -9,29 +9,30 @@ import (
 )
 
 func initDB(addrDB, pathFile string) service.Storage {
+
 	if addrDB != "" {
 		// Хранение в базе данных
 		fmt.Println("Using database storage with DSN:", addrDB)
 		// Инициализация базы данных и работа с ней
-		repo, err := db.NewPstStorage(addrDB)
+		repoDB, err := db.NewPstStorage(addrDB)
 		if err != nil {
 			fmt.Println("Fatal: ", err)
 		}
-		return repo
+		return repoDB
 	} else if pathFile != "" {
 		// Хранение в файле
 		fmt.Println("Using database storage with file:", pathFile)
 		// Инициализируем хранение в файле
-		repo, err := filestorage.NewSaveFile(pathFile)
+		repoFile, err := filestorage.NewSaveFile(pathFile)
 		if err != nil {
 			fmt.Println("Fatal: ", err)
 		}
-		return repo
+		return repoFile
 	} else {
 		// Хранение в памяти
 		fmt.Println("Using in-memory storage")
 		// Инициализация хранения в памяти
-		repo := mapstorage.NewMapURL()
-		return repo
+		repoMap := mapstorage.NewMapURL()
+		return repoMap
 	}
 }
