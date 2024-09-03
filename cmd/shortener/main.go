403d36f9c046cb7eabb2ed6b30867cbdbf7721d5
User <dagcat@mail.ru>
2024-09-03 11:09:02 +0300

    iter1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,71 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	"regexp"
+)
+
+var urlMap = map[string]string{}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+func saveURL(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	bytes, _ := io.ReadAll(r.Body)
+	urlStr := string(bytes)
+	randomPath := RandStringBytes(8)
+	urlMap[randomPath] = urlStr
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "http://localhost:8080/%v", randomPath)
+}
+
+func getURLByID(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	shortURL := r.URL.Path[1:]
+	value, ok := urlMap[shortURL]
+	if ok {
+		w.Header().Set("Location", value)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
+func processURLShortener(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Path[1:]
+	if url == "" {
+		saveURL(w, r)
+	}
+	re := regexp.MustCompile(`^[A-Za-z]{8}$`)
+	if re.MatchString(url) {
+		getURLByID(w, r)
+	}
+	w.WriteHeader(http.StatusBadRequest)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, processURLShortener)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
