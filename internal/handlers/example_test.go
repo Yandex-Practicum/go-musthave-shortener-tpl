ce81dd896991eab8c8c929e69d4eb75230dcef2c
Pavel <kamencov@gmail.com>
2024-10-23 21:19:16 +0300

    iter18

--- internal/handlers/example_test.go
+++ internal/handlers/example_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"github.com/kamencov/go-musthave-shortener-tpl/internal/logger"
 	"github.com/kamencov/go-musthave-shortener-tpl/internal/middleware"
 	serv "github.com/kamencov/go-musthave-shortener-tpl/internal/service"
@@ -25,15 +26,26 @@ func ExampleHandlers_PostURL() {
 		baseURL: baseURL,
 		logger:  loger,
 	}
+
+	// создаем body
 	payload := []byte("http://example.com")
+
+	// создаем респонс
 	w := httptest.NewRecorder()
+
+	// создаем запрос
 	r := httptest.NewRequest("POST", baseURL, bytes.NewBuffer(payload))
+
+	// создаем контекст
 	ctx := context.WithValue(r.Context(), middleware.UserIDContextKey, "userID")
 	r = r.WithContext(ctx)
 
 	// Test
 	h.PostURL(w, r)
 
+	// Print
+	fmt.Println(w.Code)
+
 	// Output:
-	// "TmMIw"
+	// 201
 }
