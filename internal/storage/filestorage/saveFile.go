37af9fe9aa0419ee2a7e084e805dd891f489df33
Pavel <kamencov@gmail.com>
2024-10-26 13:58:10 +0300

    result project

--- internal/storage/filestorage/saveFile.go
+++ internal/storage/filestorage/saveFile.go
@@ -2,21 +2,37 @@ package filestorage
 
 import "github.com/kamencov/go-musthave-shortener-tpl/internal/models"
 
-// SaveURL - функция для записи в файл.
+// SaveURL - функция для записи в файл. Старая версия.
+//func (s *SaveFile) SaveURL(shortURL, originalURL, userID string) error {
+//	Count++
+//	var events = []*Event{
+//		{
+//			UUID:        Count,
+//			ShortURL:    shortURL,
+//			OriginalURL: originalURL,
+//		},
+//	}
+//
+//	for _, event := range events {
+//		if err := s.WriteSaveModel(event); err != nil {
+//			return err
+//		}
+//	}
+//	return nil
+//}
+
+// SaveURL - функция для записи в файл.
 func (s *SaveFile) SaveURL(shortURL, originalURL, userID string) error {
 	Count++
-	var events = []*Event{
-		{
-			UUID:        Count,
-			ShortURL:    shortURL,
-			OriginalURL: originalURL,
-		},
+	event := &Event{
+		UUID:        Count,
+		ShortURL:    shortURL,
+		OriginalURL: originalURL,
 	}
 
-	for _, event := range events {
-		if err := s.WriteSaveModel(event); err != nil {
-			return err
-		}
+	// Записываем событие напрямую, избегая создания массива.
+	if err := s.WriteSaveModel(event); err != nil {
+		return err
 	}
 	return nil
 }
