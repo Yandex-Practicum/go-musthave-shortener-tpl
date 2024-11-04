f01649b23aec0f667795bf74196f872e76de9020
Pavel <kamencov@gmail.com>
2024-11-04 16:23:06 +0300

    iter20

--- internal/logger/logger_test.go
+++ internal/logger/logger_test.go
@@ -45,3 +45,81 @@ func TestWithLevel(t *testing.T) {
 		})
 	}
 }
+
+// Тест для функции WithAddSource
+func TestWithAddSource(t *testing.T) {
+	// Начальные значения LoggerOptions
+	options := &LoggerOptions{
+		AddSource: false, // По умолчанию значение false
+	}
+
+	// Вызываем функцию WithAddSource с true
+	optionFunc := WithAddSource(true)
+	optionFunc(options)
+
+	// Проверяем, что AddSource обновился до true
+	if !options.AddSource {
+		t.Errorf("expected AddSource to be true, got %v", options.AddSource)
+	}
+
+	// Вызываем функцию WithAddSource с false
+	optionFunc = WithAddSource(false)
+	optionFunc(options)
+
+	// Проверяем, что AddSource обновился до false
+	if options.AddSource {
+		t.Errorf("expected AddSource to be false, got %v", options.AddSource)
+	}
+}
+
+// Тест для функции WithIsJSON
+func TestWithIsJSON(t *testing.T) {
+	// Начальные значения LoggerOptions
+	options := &LoggerOptions{
+		IsJSON: false, // По умолчанию значение false
+	}
+
+	// Вызываем функцию WithIsJSON с true
+	optionFunc := WithIsJSON(true)
+	optionFunc(options)
+
+	// Проверяем, что IsJSON обновился до true
+	if !options.IsJSON {
+		t.Errorf("expected IsJSON to be true, got %v", options.IsJSON)
+	}
+
+	// Вызываем функцию WithIsJSON с false
+	optionFunc = WithIsJSON(false)
+	optionFunc(options)
+
+	// Проверяем, что IsJSON обновился до false
+	if options.IsJSON {
+		t.Errorf("expected IsJSON to be false, got %v", options.IsJSON)
+	}
+}
+
+// Тест для функции WithSetDefault
+func TestWithSetDefault(t *testing.T) {
+	// Начальные значения LoggerOptions
+	options := &LoggerOptions{
+		SetDefault: false, // По умолчанию значение false
+	}
+
+	// Вызываем функцию WithSetDefault с true
+	optionFunc := WithSetDefault(true)
+	optionFunc(options)
+
+	// Проверяем, что SetDefault обновился до true
+	if !options.SetDefault {
+		t.Errorf("expected SetDefault to be true, got %v", options.SetDefault)
+	}
+
+	// Вызываем функцию WithSetDefault с false
+	optionFunc = WithSetDefault(false)
+	optionFunc(options)
+
+	// Проверяем, что SetDefault обновился до false
+	if options.SetDefault {
+		t.Errorf("expected SetDefault to be false, got %v", options.SetDefault)
+	}
+}
